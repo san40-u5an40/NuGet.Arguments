4427f85b4b29e50948f23c41869b912817a0097b
san40-u5an40 <alexandr.dev2011@gmail.com>
2026-02-07 22:52:16 +0300

    Борьба с Deterministic продолжается

--- src/GlobalUsings.cs
+++ src/GlobalUsings.cs
@@ -6,5 +6,3 @@ global using System.Text.RegularExpressions;
 global using System.IO;
 
 global using san40_u5an40.Arguments;
-
-[assembly: System.Runtime.Versioning.TargetFramework(".NETCoreApp,Version=v10.0", FrameworkDisplayName = ".NET 10.0")]
